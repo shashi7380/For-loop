64d1e8ad8066f3bbb55aba209f4db8d596fc639c
shashikant <shashikant442017@gmail.com>
2021-01-08 18:33:52 +0530

    sdlfkj

---
+++ js/script.js
@@ -0,0 +1,11 @@
+alert("Click me shown in the number!");
+var greeting ="";
+var i;
+for (let i = 0; i <10; i++) {
+greeting+="</br> Thi is number="+i;
+    
+} 
+
+
+
+document.getElementById("test").innerHTML=greeting;
